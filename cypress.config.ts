5b7bf6a5095891b1d3a8a1642cb85e2d0337a914
Mani Movassagh <mani.mgh@gmail.com>
2023-03-30 21:30:52 +0200

    ordering works

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,5 @@
 const { defineConfig } = require("cypress");
-
+import { ordering } from "./cypress/orderring/ordeing";
 module.exports = defineConfig({
 
   e2e: {
@@ -9,6 +9,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require('@cypress/grep/src/plugin')(config);
+      config.specPattern = ordering
       return config;
     },
   },
---
+++ cypress/orderring/ordeing.ts
@@ -0,0 +1,6 @@
+export const ordering = [
+    "cypress/e2e/mani.cy.ts",
+    "cypress/e2e/tt.cy.ts",
+    "cypress/e2e/spec.cy.ts",
+    "cypress/e2e/second.cy.ts"
+]
