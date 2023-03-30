c9bb555aad2ac8781aafd165673f6ecd8b95e164
Mani Movassagh <mani.mgh@gmail.com>
2023-03-30 21:33:01 +0200

    add any to config

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,7 @@ module.exports = defineConfig({
 
     baseUrl: 'http://localhost:3000/',
 
-    setupNodeEvents(on, config) {
+    setupNodeEvents(on:any, config:any) {
       // implement node event listeners here
       require('@cypress/grep/src/plugin')(config);
       config.specPattern = ordering
