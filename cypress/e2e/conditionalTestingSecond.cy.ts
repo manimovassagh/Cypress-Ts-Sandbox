fe28d2ebecddfe6380bd63cff1f570a05d7f19bc
Mani Movassagh <mani.mgh@gmail.com>
2023-03-31 22:43:44 +0200

    conditional testing

---
+++ cypress/e2e/conditionalTestingSecond.cy.ts
@@ -0,0 +1,17 @@
+
+it('Second way for conditional testing', () => {
+
+    cy.visit('/').wait(100)
+
+    cy.get('.todo')
+        .should('have.length.gte', 0)
+        .then(function ($todos) {
+            if (!$todos.length) {
+                cy.log('nothing to delete')
+                return
+            }
+            cy.wrap($todos)
+                .find('.destroy')
+                .click({ force: true, multiple: true })
+        })
+});
